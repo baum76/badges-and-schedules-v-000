dbd1a0285c8d035d7bd716ec4eedd60c4082b7c2
Matt Baumgarten <mattybaumgarten@gmail.com>
2018-03-22 21:53:42 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+require "pry"
+
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  badges = []
+  attendees.each {|attendee| badges << badge_maker(attendee)}
+  return badges
+end
+
+def assign_rooms(attendees)
+  attendees.map.with_index {|attendee, room_number| "Hello, #{attendee}! You'll be assigned to room #{room_number + 1}!"}
+end
+
+def printer(attendees)
+  attendees.each_line do |badges_and_room_assignments|
+    batch_badge_creator(attendees)
+    assign_rooms(attendees)
+  end
+end
